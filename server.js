9401ec09181333f745ef0ea984eeab3824b059fa
dnaglobalstaffing <dnaglobalstaffing@gmail.com>
2026-02-07 19:45:18 +0530

    Update all website files and service pages

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-/* =======================
-   CLEAN URL ROUTES
-======================= */
+ 
+
 app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "contact.html"));
 });
@@ -55,4 +54,4 @@ app.post("/request-service", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
-});
+
